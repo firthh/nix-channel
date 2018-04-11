751065d539358d00ef0d6d20b4e944cc8e25db2e
Hugo <hugo.firth@gmail.com>
2018-04-11 15:21:41 +0100

    Trying to turn this into a channel

--- default.nix
+++ default.nix
@@ -1,11 +1,12 @@
 { system ? builtins.currentSystem }:
+
 let
    pkgs = import <nixpkgs> { inherit system; };
 
    callPackage = pkgs.lib.callPackageWith (pkgs // self);
 
-   self = {                                                                             
-      postgres_9_4_17 = callPackage ./postgres { };
-      node_9_11_1 = callPackage ./node {};
+   self.developer = {                                                                             
+      postgresql-9_4_17 = callPackage ./postgres/default.nix {};
+      nodejs-9_11_1 = callPackage ./node/default.nix {};
    };
    in self
--- postgres/default.nix
+++ postgres/default.nix
@@ -1,10 +1,11 @@
 { stdenv, fetchurl, pkgs }:
-
-pkgs.postgresql94.overrideAttrs(oldAttrs: {
-     version = "9.4.17";
-     name = "postgresql-9.4.17";
+let
+  postgresVersion = "9.4.17";
+in pkgs.postgresql94.overrideAttrs(oldAttrs: {
+     version = postgresVersion;
+     name = "postgresql-${postgresVersion}";
      src = fetchurl {
-      url = "mirror://postgresql/source/v9.4.17/postgresql-9.4.17.tar.bz2";
-      sha256 = "7a320cd335052b840d209dc9688f09965763351c590e3cc7bf577591179fd7c6";
+         url = "mirror://postgresql/source/v${postgresVersion}/postgresql-${postgresVersion}.tar.bz2";
+         sha256 = "7a320cd335052b840d209dc9688f09965763351c590e3cc7bf577591179fd7c6";
      };
-   })
+})
